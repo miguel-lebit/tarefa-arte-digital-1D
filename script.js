902a355e6c209b7e0b5e05b73181aa13af12e01e
miguel-lebit <miguel.lebit@escola.pr.gov.br>
2026-09-18 10:58:00 -0300

    atualizado

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+const botoesCurtir = document.querySelectorAll(".Curtir");
+botoesCurtir.forEach(function(botaoCurtir) {
+    let curtir = false;
+    botaoCurtir.addEventListener("Click", curtir);
+function curtir(){
+    const contador = botaoCurtir.querySelector("span");
+    if(curtir === false){
+        contador.textContent++;
+        curtiu = true;}
+        else{
+            contador.textContent--;
+            curtiu = false;
+        }
+}   
+});
